362ad068938ea8accf900a0de57cec3e09f1b936
Nikhil Ghosh <nghosh@cloudflare.com>
2024-10-26 17:14:47 -0400

    add a few more users for testing

--- duckduckgo/schema.sql
+++ duckduckgo/schema.sql
@@ -1,7 +1,18 @@
+-- Create the users table
 CREATE TABLE users (
     id INTEGER PRIMARY KEY,
     balance INTEGER
 );
 
-INSERT INTO users (id, balance) VALUES (1, 100);
-INSERT INTO users (id, balance) VALUES (2, 200);
+-- Insert 10 users with balance of 1000 each
+INSERT INTO users (id, balance) VALUES
+    (1, 1000),
+    (2, 1000),
+    (3, 1000),
+    (4, 1000),
+    (5, 1000),
+    (6, 1000),
+    (7, 1000),
+    (8, 1000),
+    (9, 1000),
+    (10, 1000);
