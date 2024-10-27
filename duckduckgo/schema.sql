1b19bd8c4b0b5918a0f2bb12bc5dd920779dc61d
Nikhil Ghosh <nghosh@cloudflare.com>
2024-10-27 19:04:34 -0400

    more experiments

--- duckduckgo/schema.sql
+++ duckduckgo/schema.sql
@@ -4,6 +4,6 @@ CREATE TABLE users (
     balance INTEGER
 );
 
--- Insert (10, 100, 1000) users with a balance of 1000 each
+-- Insert (10, 100, 1k, 10k, 100k, 1M) users with a balance of 1000 each
 INSERT INTO users (id, balance)
 SELECT generate_series AS id, 1000 FROM generate_series(1, 1000);
